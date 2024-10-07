55c4d79e2cedfa2e7c9712c8449bd76f90d30f53
cerenacar1 <ceremiacar@gmail.com>
2024-10-07 22:55:13 +0300

    React Projesi 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import 'bulma/css/bulma.css';
 import './App.css';
+import Course from './Course';
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap5.png';
+import Ccsharp from './images/ccsharp.png';
+import KompleWeb from './images/kompleweb.jpg';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='hero is-link'>
+        <div className='hero-body'>
+          <p className='title'>Kurslarım</p>
+        </div>
+      </section>
+      <div className='container'>
+      <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+            <Course 
+              image = {Angular}
+              title = "Angular"
+              description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            />
+          </div>
+          <div className='column'>
+            <Course 
+              image = {Bootstrap}
+              title = "Bootstrap 5"
+              description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            />
+          </div>
+          <div className='column'>
+            <Course 
+              image = {Ccsharp}
+              title = "Komple Web"
+              description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            />
+          </div>
+          <div className='column'>
+            <Course 
+              image = {KompleWeb}
+              title = "Frontend"
+              description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            />
+          </div>
+        </div>
+      </section>
+      </div>
+
+
+
     </div>
   );
 }
